d18e393ac95b8ccd92b12d7215873fc8965e41b5
darvid7 <david.anthony.lei@gmail.com>
2018-05-16 18:45:10 +1000

    Fix song parser

--- data_parser/parse_songs.py
+++ data_parser/parse_songs.py
@@ -1,10 +1,15 @@
 SONG_FILE = "../billboard.csv"
 
 def parse_songs(song_file):
-    song_data = [] # List of (name, year, artist, lyrics) tuples.
-    with open(song_file, 'r') as fh:
+    song_data = []  # List of (name, year, artist, lyrics) tuples.
+    with open(song_file, 'r', encoding='utf-8') as fh:
         for line in fh:
             tokens = line.split(',', 4)
             _, song, year, artist, lyrics = tokens
             song_data.append((song, year, artist, lyrics))
     return song_data
+
+if __name__ == "__main__":
+    songs = parse_songs(SONG_FILE)
+    for s in songs:
+        print(s)
